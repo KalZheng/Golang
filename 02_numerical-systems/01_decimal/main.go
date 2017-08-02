442a1e867202aaa0343da450fd06eeff5304e881
Kal Zheng <kalsing456@gmail.com>
2017-08-02 14:48:09 -0600

    section finish with loop

---
+++ 02_numerical-systems/01_decimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(42)
+	//fmt.printf("%d - %b", 42, 42)
+}
---
+++ 02_numerical-systems/02_decimal/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println(42)
+	fmt.Printf("%d - %b", 42, 42)
+}
